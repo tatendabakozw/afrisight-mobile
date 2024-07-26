5c5c47ecb197f82f50fd76cf0204e72191672e06
tatendabakozw <tatendabakozw@gmail.com>
2024-07-26 14:29:54 +0200

    feat: added notification header

--- app/(modals)/notification.tsx
+++ app/(modals)/notification.tsx
@@ -1,11 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import tw from "twrnc";
+import { useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const Notification = () => {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Notification</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={[
+        tw`bg-zinc-50 px-4 w-full flex-1 gap-4 pb-4`,
+        {
+          paddingTop: insets.top,
+        },
+      ]}
+    >
+      <View style={tw`flex flex-row gap-4 py-2`}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={tw`bg-zinc-100 rounded-full p-2`}
+        >
+          <Ionicons name="arrow-back-sharp" size={24} color="#71717a" />
+        </TouchableOpacity>
+        <View style={tw`flex-1`} />
+        <TouchableOpacity style={tw` rounded-full p-2`}>
+          <Text style={tw`text-[${Colors.light.primary}]`}>
+            Mark all as read
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
