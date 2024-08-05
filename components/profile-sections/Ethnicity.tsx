2169bd7a8468278a3ff7687ca85417119f8c7337
tatendabakozw <tatendabakozw@gmail.com>
2024-08-05 12:31:37 +0200

    feature: added hobbies

--- components/profile-sections/Ethnicity.tsx
+++ components/profile-sections/Ethnicity.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import tw from "twrnc";
+import Colors from "@/constants/Colors";
 
 type Props = {};
 
 const Ethnicity = (props: Props) => {
+  const [selectedEthinicity, setSelectedEthnicity] = useState<any>();
+  const ethnicities = [
+    { name: "Asian", _id: "asian" },
+    { name: "Black/African", _id: "african" },
+    { name: "Middle Eastern/ North African", _id: "middle-eastern" },
+    { name: "Hispanic/Latino", _id: "hiispanic" },
+    { name: "Pacific Islander", _id: "pacific-finder" },
+    { name: "Mixed Race", _id: "mixed" },
+    { name: "Native American/First Nations", _id: "native-american" },
+    { name: "White/Caucasian", _id: "white" },
+    { name: "Other", _id: "other" },
+  ];
   return (
-    <View>
-      <Text>Ethnicity</Text>
+    <View style={tw`flex flex-row flex-wrap gap-6`}>
+      {ethnicities.map((item) => (
+        <TouchableOpacity
+          onPress={() => setSelectedEthnicity(item)}
+          activeOpacity={0.7}
+          style={tw`${
+            selectedEthinicity?._id === item._id
+              ? `bg-[${Colors.light.primary}] `
+              : "bg-zinc-200 "
+          }  py-2 px-4 rounded-full`}
+          key={item._id}
+        >
+          <Text style={tw`text-zinc-700`}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
--- components/profile-sections/Hobbies.tsx
+++ components/profile-sections/Hobbies.tsx
@@ -1,12 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import tw from "twrnc";
+import Colors from "@/constants/Colors";
 
 type Props = {};
 
 const Hobbies = (props: Props) => {
+  const [selectedHobbies, setSelectedHobbies] = useState<any[]>([]);
+  const hobbies = [
+    { name: "Reading", _id: "reading" },
+    { name: "Traveling", _id: "traveling" },
+    { name: "Cooking", _id: "cooking" },
+    { name: "Gaming", _id: "gaming" },
+    { name: "Hiking", _id: "hiking" },
+    { name: "Swimming", _id: "swimming" },
+    { name: "Painting", _id: "painting" },
+    { name: "Music", _id: "music" },
+    { name: "Photography", _id: "photography" },
+  ];
+
+  const toggleHobbySelection = (hobbyId: string) => {
+    setSelectedHobbies((prevSelectedHobbies) =>
+      prevSelectedHobbies.includes(hobbyId)
+        ? prevSelectedHobbies.filter((id) => id !== hobbyId)
+        : [...prevSelectedHobbies, hobbyId]
+    );
+  };
+
   return (
-    <View>
-      <Text>Hobbies</Text>
+    <View style={tw`flex flex-row flex-wrap gap-6`}>
+      {hobbies.map((item) => (
+        <TouchableOpacity
+          onPress={() => toggleHobbySelection(item._id)}
+          activeOpacity={0.7}
+          style={tw`${
+            selectedHobbies.includes(item._id)
+              ? `bg-[${Colors.light.primary}]`
+              : "bg-zinc-200"
+          } py-2 px-4 rounded-full`}
+          key={item._id}
+        >
+          <Text style={tw`text-zinc-700`}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
